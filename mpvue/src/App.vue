80c5f42c04251603a0da362a26a081fc6d8ce816
Ling Zhang <zhangling.rd@gmail.com>
2018-11-18 20:13:19 +0800

    项目配置

--- mpvue/src/App.vue
+++ mpvue/src/App.vue
@@ -6,7 +6,16 @@ export default {
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    console.log('app created and cache logs by setStorageSync')
+		console.log('app created and cache logs by setStorageSync')
+
+		// 云开发检测
+		if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }
   }
 }
 </script>
